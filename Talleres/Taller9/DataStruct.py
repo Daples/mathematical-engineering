b9b7a576f4ac47f0e183759649244ad8d8f4ed66
eafit-201710008101 <jscardenar@eafit.edu.co>
2017-09-19 15:22:04 -0500

    Taller9V2.0

--- Talleres/Taller9/DataStruct.py
+++ Talleres/Taller9/DataStruct.py
@@ -1,54 +1,63 @@
 class HashTable:
-	def __init__(self, size = 100):
-		self.size = size
-		self.table = [None for e in range(size)]
+    def __init__(self, size=100):
+        self.size = size
+        self.table = [None for e in range(size)]
 
-	def _hash_function(self, key):
-		if type(key) == str:
-			acum = 0
-			i = 1
-			n = len(key)
-			for let in key:
-				acum += ord(let)*(31**(n - i))
-				i += 1
-			return (acum)%(self.size)
-		else:
-			print("The key passed is not a string")
+    def _hash_function(self, key):
+        if type(key) == str:
+            acum = 0
+            i = 1
+            n = len(key)
+            for let in key:
+                acum += ord(let)*(31**(n - i))
+                i += 1
+            return acum % self.size
+        else:
+            print("The key passed is not a string")
 
-	def add(self, keys):
-		for key in keys:
-			hash_value = self._hash_function(key["key"])
-			if self.table[hash_value] == None:
-				self.table[hash_value] = []
-			self.table[hash_value].append(key["value"])
+    def add(self, keys):
+        for key in keys:
+            hash_value = self._hash_function(key["key"])
+            if self.table[hash_value] is None:
+                self.table[hash_value] = []
+            self.table[hash_value].append(key["value"])
 
-	def get(self, key):
-		hash_value = self._hash_function(key)
-		if self.table[hash_value] == None:
-			print("There's no value assinged to that key")
-			return None
-		else:
-			if len(self.table[hash_value]) == 1:
-				return self.table[hash_value][0]
-			else:
-				return self.table[hash_value]
+    def get(self, key):
+        hash_value = self._hash_function(key)
+        if self.table[hash_value] is None:
+            print("There's no value assinged to that key")
+            return None
+        else:
+            if len(self.table[hash_value]) == 1:
+                return self.table[hash_value][0]
+            else:
+                return self.table[hash_value]
 
-	def arcget(self, value):
-		self.table.index(value)
+    # 4
+    def is_key(self, key):
+        if self.get(key) is None:
+            return False
+        else:
+            return True
 
+    # 5
+    def is_value(self, value):
+        for i in range(self.size):
+            if self.table[i] is not None:
+                for j in range(len(self.table[i])):
+                    if self.table[i][j] == value:
+                        return True
 
-# 4...
-def iscompany(hash, key):
-	if hash.get(key) != None:
-		return True
-	else:
-		return False
+        return False
 
-myhash = HashTable(25)
-myhash.add([{"key": "Google", "value": "USA"},
-			{"key": "La locura", "value": "COL"},
-			{"key": "Nokia", "value": "FIN"},
-			{"key": "Sony", "value": "JAP"}])
-
-print(str(iscompany(myhash, "Google")))
-
+my_hash = HashTable(25)
+my_hash.add([{"key": "Google", "value": "USA"},
+            {"key": "La locura", "value": "COL"},
+            {"key": "Nokia", "value": "FIN"},
+            {"key": "Sony", "value": "JAP"}])
+"""
+For the exercise 4 and 5 use the is_key and is_value function respectively to answer both of the questions.
+This workshop is made by:
+    - David Plazas
+    - Juan Sebastián Cárdenas
+"""
