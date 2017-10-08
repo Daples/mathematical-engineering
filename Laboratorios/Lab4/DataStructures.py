c0fa5cf1b3f5d43073f83efa8ebd9017da2e34cf
eafit-201710008101 <jscardenar@eafit.edu.co>
2017-10-08 00:42:51 -0500

    hhh

--- Laboratorios/Lab4/DataStructures.py
+++ Laboratorios/Lab4/DataStructures.py
@@ -6,23 +6,6 @@ class LinkedList(object):
             self.next = None
             self.prev = None
 
-        def set_next(self, obj):
-            self.next = obj
-
-        def set_prev(self, obj):
-            self.prev = obj
-
-        def set(self, node):
-            self.node = node.node
-            self.set_next(node.get_next())
-            self.set_prev(node.get_prev())
-
-        def get_prev(self):
-            return self.prev
-
-        def get_next(self):
-            return self.next
-
         def __str__(self):
             return str(self.node)
 
@@ -32,16 +15,28 @@ class LinkedList(object):
         self.last = None
 
     def __len__(self):
+        """
+        :return: the size of the linked list
+        """
         return self.int_size
 
     def insert_multi(self, *objs):
+        """
+        :param objs: objects to insert in the linked list
+        :return: nothing
+        """
         for obj in objs:
             self.insert(obj, self.int_size)
 
     def insert(self, obj, i=0):
+        """
+        :param obj: the object you want to insert
+        :param i: the index you want to insert the object
+        :return: nothing
+        """
         if i > self.int_size:
             print("Index out of bounds")
-            return
+            return None
         else:
             obj_node = LinkedList.Node(obj)
             mov_obj = self.first
@@ -49,19 +44,19 @@ class LinkedList(object):
                 self.first = obj_node
                 self.last = obj_node
                 self.int_size += 1
-                return
+                return None
             elif i == 0:
                 self.first.prev = obj_node
                 obj_node.next = self.first
                 self.first = obj_node
                 self.int_size += 1
-                return
+                return None
             elif i == self.int_size:
                 obj_node.prev = self.last
                 self.last.next = obj_node
                 self.last = obj_node
                 self.int_size += 1
-                return
+                return None
             elif i < self.int_size / 2:
                 for j in range(i):
                     if j <= i - 1:
@@ -87,8 +82,13 @@ class LinkedList(object):
                 last.prev = obj_node
 
             self.int_size += 1
+            return obj
 
     def get(self, i):
+        """
+        :param i: the index you want to get the object
+        :return: the object at index i
+        """
         if i >= self.int_size:
             print("The index passed doesn't exist on the list")
             return None
@@ -106,12 +106,14 @@ class LinkedList(object):
 
             return temp_node.node
 
-    def get_iterator(self):
-        return self.it
-
     def remove(self, i):
+        """
+        :param i: the index of the object you want to remove
+        :return: the object removed
+        """
         if i >= self.int_size:
             print("Index out of bounds")
+            return None
         else:
             if self.int_size == 1:
                 self.first = None
@@ -119,15 +121,17 @@ class LinkedList(object):
                 self.int_size -= 1
                 return
             elif i == 0:
+                erased = self.first
                 self.first.next.prev = None
                 self.first = self.first.next
                 self.int_size -= 1
-                return
+                return erased.node
             elif i == self.int_size - 1:
+                erased = self.last
                 self.last.prev.next = None
                 self.last = self.last.prev
                 self.int_size -= 1
-                return
+                return erased.node
             elif i > self.int_size/2:
                 temp_node = self.last
                 j = self.int_size - 1
@@ -138,16 +142,23 @@ class LinkedList(object):
                 temp_node = self.first
                 for j in range(i):
                     temp_node = temp_node.next
-
+            erased = temp_node
             temp_node.prev.next = temp_node.next
             temp_node.next.prev = temp_node.prev
+            return erased.node
 
     def clear(self):
+        """
+        :return: nothing
+        """
         self.first = None
         self.last = None
         self.int_size = 0
 
     def __str__(self):
+        """
+        :return: a string version of the linked list
+        """
         temp_obj = self.first
         acum = ""
         while temp_obj is not None:
@@ -159,28 +170,48 @@ class LinkedList(object):
         return acum
 
     def __contains__(self, item):
+        """
+        :param item: the item you want to search for
+        :return: if the item is in the list
+        """
         for i in self:
             if i is item:
                 return True
         return False
 
     def to_list(self):
+        """
+        :return: a iterable version of the linked list
+        """
         temp_object = self.first
         while temp_object is not None:
             yield temp_object.node
             temp_object = temp_object.next
 
     def __iter__(self):
+        """
+        :return: a iterable object of the list
+        """
         return self.to_list()
 
     def size(self):
+        """
+        :return: the size of the list
+        """
         return self.int_size
 
     def get_first(self):
+        """
+        :return: the value of the first node
+        """
         return self.first.node
 
     def get_last(self):
+        """
+        :return: the value of the last node
+        """
         return self.last.node
+
 
 class Queue:
 
@@ -188,53 +219,105 @@ class Queue:
         self.ll = LinkedList()
 
     def push(self, obj):
+        """
+        :param obj: the object you want to insert
+        :return: nothing
+        """
         self.ll.insert(obj, 0)
 
     def push_multi(self, *objs):
+        """
+        :param objs: the objects to insert in the queue in the order they arrived
+        :return: nothing
+        """
         for obj in objs:
             self.push(obj)
 
     def peek(self):
-        return self.ll.last
+        """
+        :return: the item that first inserted it the queue
+        """
+        return self.ll.get_last()
 
     def pop(self):
+        """
+        :return: the element that first was inserted in the queue
+        """
         temp_obj = self.ll.last
         self.ll.remove(self.ll.size() - 1)
         return temp_obj
 
     def size(self):
+        """
+        :return: the size of the queue
+        """
         return self.ll.size()
 
     def __str__(self):
+        """
+        :return: a string version of the queue
+        """
         return str(self.ll)
 
 
 class Stack:
 
     def __init__(self, size):
+        """
+        :param size: maximum size of the stack
+        """
         self.max_size = size
         self.ll = LinkedList()
 
     def push_multi(self, *objs):
+        """
+        :param objs: the objects you want to insert
+        :return: nothing
+        """
         for obj in objs:
-            self.ll.insert(obj, 0)
+            self.ll.insert(obj, self.ll.size())
 
     def push(self, obj):
+        """
+        :param obj: the object you want to insert
+        :return: nothing
+        """
         if self.ll.size() < self.max_size:
-            self.ll.insert(obj, 0)
+            self.ll.insert(obj, self.ll.size())
         else:
             print("Stack overflow")
 
     def peek(self):
-        return self.ll.first.node
+        """
+        :return: the object that last entered the stack
+        """
+        return self.ll.get_last()
 
     def pop(self):
-        temp_object = self.ll.first.node
-        self.ll.remove(0)
+        """
+        :return: the first element in the stack
+        """
+        temp_object = self.ll.get_last()
+        self.ll.remove(self.ll.size() - 1)
         return temp_object
 
     def size(self):
+        """
+        :return: the size of the stack
+        """
         return self.ll.size()
 
     def __str__(self):
-        return str(self.ll)
+        """
+        :return: a string version of the stack
+        """
+        acum = " "
+        for item in self.ll:
+            acum += str(item) + " "
+        return acum
+
+    def clear(self):
+        """
+        :return: nothing
+        """
+        self.ll.clear()
