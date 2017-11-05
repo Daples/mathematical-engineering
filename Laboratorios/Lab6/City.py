41a84ded788dadb1043af7d172bf16ac8c52126f
Daples <daples1@gmail.com>
2017-11-05 14:48:08 -0500

    shhh

--- Laboratorios/Lab6/City.py
+++ Laboratorios/Lab6/City.py
@@ -1,2 +1,8 @@
+#from DataStructure import Graph
 class City:
-    
+    def readFile():
+        G = Graph()
+        f = open('test.txt','r') #Format -> ID  CoordX  CoordY  Name
+        for line in f:
+            lineL = line.split(' ')
+            
