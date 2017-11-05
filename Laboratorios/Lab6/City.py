e5f25e76b35c3e43c6e6acaa37f8c08731310d49
Daples <daples1@gmail.com>
2017-11-05 17:15:53 -0500

    hhh

--- Laboratorios/Lab6/City.py
+++ Laboratorios/Lab6/City.py
@@ -1,10 +1,14 @@
-from DataStructures import DigraphAL, DigraphAM
+from DataStructures import DigraphAL
 
 class City:
     def readFile():
-        f = open('test.txt','r') #Format -> ID  CoordX  CoordY  Name
+        f1 = open('medellin.txt','r') #Format -> ID  CoordX  CoordY  Name
+        IDs = []
+        n = [i for i in range(310153)]
         for line in f:
             lineL = line.split(' ')
-
-        print(lineL)
+            IDs.append(lineL[0])
+        IDs_Index = dict(zip(IDs,n))
+        G = DigraphAL(310153)
+        f2 = open('connection.rtf','r') #Format -> ID ID distancia nombre
 City.readFile()
