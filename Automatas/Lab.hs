f42aa9b8843580f34b6cfd876265f093e480624e
Daples <dplazas@eafit.edu.co>
2018-09-02 16:10:55 -0500

    shhh

--- Automatas/Lab.hs
+++ Automatas/Lab.hs
@@ -1,8 +1,12 @@
+{- | Converts a deterministic finite automaton (DFA) into a regular expression (Regex), using the DFA2Regex function. -}
+module DFA2Regex where
+
 import Data.Set as Set
 import Data.Map as Map
-import DFA 
+import DFA
 import Regex
 
+-- | Simplifies the output regex from the given DFA.
 simplify :: Regex a -> Regex a
 simplify Empty = Empty
 simplify Epsilon = Epsilon
@@ -28,9 +32,11 @@ simplify (Plus (Symbol b) c) = Plus (Symbol b) (simplify c)
 simplify (Plus b (Symbol c)) = Plus (simplify b) (Symbol c)
 simplify (Plus a b) = Plus (simplify a) (simplify b)
 
+-- | Extracts the set of states of the given DFA.
 states :: Ord s => DFA s c -> Set s
 states dfa = Set.fromList $ Map.keys (delta dfa)
-                                         
+
+-- |
 tran :: Eq s => Map c s -> s -> Regex c
 tran map1 j = tran' (Map.assocs map1) j where
 tran' :: Eq s => [(c,s)] -> s -> Regex c
@@ -55,6 +61,7 @@ dfa2Regex' qs (i,j) k d
                     rkj = simplify (dfa2Regex' qs (k,j) (k-1) d)
                 in simplify (Plus rij (Dot rik (Dot (Star rkk) rkj)))
 
+-- | Converts an input DFA into a Regex.
 dfa2Regex :: Ord s => DFA s c -> Regex c
 dfa2Regex dfa = dfa2Aux dfa (Set.elems (accepting dfa)) where
 dfa2Aux :: Ord s => DFA s c -> [s] -> Regex c
@@ -63,44 +70,3 @@ dfa2Aux dfa (x : xs) =
         let reg1 = simplify (dfa2Regex1 dfa x)
             reg2 = simplify (dfa2Aux dfa xs)
         in simplify (Plus reg1 reg2)
-
-example1 :: DFA Int Int
-example1 = trans (1,1,0) .
-  trans (1,2,1) .
-  trans (2,3,0) .
-  trans (2,3,1) .
-  trans (3,4,0) .
-  trans (3,4,1) .
-  trans (4,1,0) .
-  trans (4,4,1) .
-  accept 2 .
-  accept 3 .
-  accept 4 $ initDFA 1
-
-example2 :: DFA Int Int
-example2 = trans (1,1,0) .
-   trans (1,2,1) .
-   trans (2,1,0) .
-   trans (2,2,1) .
-   accept 2 $ initDFA 1
-
-example3 :: DFA Int Int
-example3 = trans (1,1,1) .
-  trans (1,2,0) .
-  trans (2,3,1) .
-  trans (2,1,1) .
-  trans (3,1,0) .
-  trans (3,1,1) .
-  accept 3 $ initDFA 1
-
-example4 :: DFA Int Int
-example4 = trans (1,1,1) .
-  trans (1,2,0) .
-  trans (2,1,0) .
-  trans (2,3,1) .
-  trans (3,3,0) .
-  trans (3,3,1) .
-  accept 3 $ initDFA 1
-
-main :: IO()
-main = print $ (dfa2Regex example4)
