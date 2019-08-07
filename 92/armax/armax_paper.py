55081c4ed6cc4cba494f6d542739b8c69bf84b2b
juanscr <jcardenasr123@gmail.com>
2019-08-07 18:51:50 -0500

    Final version code.

--- 92/armax/armax_paper.py
+++ 92/armax/armax_paper.py
@@ -1,8 +1,17 @@
 import numpy as np
+import scipy.stats as sp
 import matplotlib.pyplot as plt
 
 
-data_file = open('data.txt','r')
+def sim(a, t1):
+    y = np.zeros(t1, dtype=float)
+    y[0] = 480
+    for t1 in range(1, t1):
+        y[t1] = 237.565 + 0.426 * y[t1 - 1] + c.dot(a[t1 - 1:t1 + 1]) + b.dot(d[:, t1])
+    return y
+
+
+data_file = open('data.txt', 'r')
 
 data = data_file.read()
 data = data.split('\n')
@@ -12,23 +21,20 @@ d = np.array([data[0].split(','),
               data[2].split(','),
               data[3].split(',')], dtype=float)
 
-# Sim Time
-T = 182
-
 b = np.array([8.9087, -1.557, 31.919, -2.045])
 c = np.array([1, 0.153])
 
-# Noise
-a = np.random.normal(0, 1, T)
+t = 182
 
-Y = np.zeros(T, dtype=float)
-Y[0] = 480
-for t in range(1, T):
-    print(str(t))
-    Y[t] = 237.565 + 0.426*Y[t-1] + c.dot(a[t-1:t+1]) + b.dot(d[:, t])
+# Noise
+a1 = np.random.normal(0, 1, t)
+a2 = sp.t.rvs(0.7, size=t)
+
+y1 = sim(a1, t)
+y2 = sim(a2, t)
 
 plt.figure(1)
-plt.plot(Y)
-plt.figure(2)
-plt.plot(d[1,0:31])
+plt.plot(y1, "r", label="Normal")
+plt.plot(y2, label="T-Student")
+plt.legend()
 plt.show()
--- 92/armax/data_gen.py
+++ 92/armax/data_gen.py
@@ -6,35 +6,60 @@ import numpy as np
 # d8 - Insolation duration
 # d9 - humidity
 
-
-data_file = open('data.txt','w')
-
-T = 182
-d1 = np.random.normal(28.5, 1.5, (T,1))
-d8 = np.random.normal(9, 1.5, (T,1))
-d9 = np.random.normal(75, 2, (T,1))
-
-
-def runge_kutta(f1, y0, T1):
+def runge_kutta(f1, y0, t1):
     h = 0.01
-    n = int(T1/h)
+    n = int(t1 / h)
     y1 = np.zeros((len(y0), n))
     y1[:, 0] = y0
-    for i in range(1, n):
-        yf = y1[:, i - 1]
+    for i1 in range(1, n):
+        yf = y1[:, i1 - 1]
         k1 = f1(yf)
         k2 = f1(yf + k1*h/2)
         k3 = f1(yf + k2*h/2)
         k4 = f1(yf + k3*h)
         m = h*(k1 + 2*k2 + 2*k3 + k4)/6
-        y1[:, i] = y1[:, i - 1] + m
+        y1[:, i1] = y1[:, i1 - 1] + m
 
-    y = np.zeros((T1, 1))
+    y = np.zeros((t1, 1))
     for j in range(0, n, int(1/h)):
         y[int(j*h), 0] = y1[2, j]
 
-    return y
+    return y*3
 
+
+def extract_data(str1):
+    df = open("data_paper.txt", "r")
+    found = False
+    l1 = []
+    for line in df.readlines():
+        line = line.replace("\n", "")
+        if found and line != "":
+            l1.append(float(line))
+        elif found:
+            break
+
+        if line == str1:
+            found = True
+
+    df.close()
+
+    return [np.mean(l1), np.std(l1)]
+
+
+def gen_norm(mv):
+    return np.random.normal(mv[0], mv[1], (T, 1))
+
+
+data_file = open('data.txt','w')
+
+
+T = 182
+strings = ["d1", "d8", "d9"]
+data = []
+for str2 in strings:
+    data.append(extract_data(str2))
+
+data = list(map(gen_norm, data))
 
 a = 0.29
 b = 0.14
@@ -50,7 +75,7 @@ d7 = runge_kutta(f, np.array([0.72, 1.28, 0.21]), T)
 #    d7[i][0] = max(d7[i][0],0)
 
 # matrix
-data = [d1, d7, d8, d9]
+data.insert(1, d7)
 for i in data:
     np.ndarray.tolist(i)
 
@@ -61,3 +86,5 @@ for i in data:
     s = s[0:len(s)-1]
     data_file.write(s + '\n')
     s = ''
+
+data_file.close()
