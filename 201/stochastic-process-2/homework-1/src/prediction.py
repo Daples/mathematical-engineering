9e4721f1ccc27a40e9dae9cdec6b4ae2f7697e9c
daples <daples1@gmail.com>
2020-04-11 20:30:53 -0500

    [ stocastic-processes-2 ] optimization code.

--- 201/stochastic-process-2/homework-1/src/prediction.py
+++ 201/stochastic-process-2/homework-1/src/prediction.py
@@ -125,17 +125,18 @@ def bandwidths(f1, g1, mu1, n1, plot=False, linewidth=0.5):
 
 
     # # Prediction Bands
-    res = prediction_bands(series_c, 0.9, t1s)
+    res = prediction_bands(series_c, 0.05, t1s)
     print(res.x)
     upper = res.x[0] * (t1s - t_final) + initial_condition
     lower = res.x[1] * (t1s - t_final) + initial_condition
-    plt.plot(t1s, upper)
-    plt.plot(t1s, lower)
     for j in range(n1):
         if j == n1 - 1:
             plt.plot(t1s, series_c[j, 0, :].transpose(), color='grey', alpha=0.25, linewidth=linewidth, label='Trayectories')
         plt.plot(t1s, series_c[j, 0, :].transpose(), color='grey', alpha=0.25, linewidth=linewidth)
+    plt.plot(t1s, upper)
+    plt.plot(t1s, lower)
     plt.show()
+
     # # # Mean Confidence Bands
     # lower, mean, upper = mean_band(series_c, 0.01)
     #
@@ -164,7 +165,6 @@ def mean_band(time_series, alpha1):
     width = S * Za / (n ** 0.5)
     lower = np.exp(y_bar - width)
     upper = np.exp(y_bar + width)
-
     means = time_series[:, 0, :].mean(axis=0)
     return lower, means, upper
 
@@ -180,16 +180,24 @@ def prediction_bands(time_series, alpha1, t):
         for j in range(time_series.shape[2]):
             t_val = t_final + j * delta_t
             probs_aux0 = time_series[:, 0, j]
-            probs_aux = probs_aux0[probs_aux0 >= a[1] * (t_val - t_final) + x0]
-            probs[j] = probs_aux[probs_aux <= a[0] * (t_val - t_final) + x0].size / time_series.shape[0]
-        return np.mean(probs) - lb
+            probs_aux1 = probs_aux0[probs_aux0 >= a[1] * (t_val - t_final) + x0]
+            probs[j] = probs_aux1[probs_aux1 <= a[0] * (t_val - t_final) + x0].size / time_series.shape[0]
+        return np.mean(probs)
 
-    a0 = np.array([14/365, -2/365])
+
+    a0 = np.array([time_series.flatten().max(), time_series.flatten().min()]) / 365
     lb = 1 - alpha1
-    bnds = ((0, 1), (-0.1, 0.1))    
-    cons = ({'type': 'eq', 'fun': prob}, {'type': 'ineq', 'fun': lambda a: a[0] - a[1]})
-    res = opt.minimize(of, a0, constraints=cons, bounds=bnds)
-    print('Constraint', str(prob(res.x) + lb))
+    bnds = ((0, 1), (-0.1, 0))
+
+    # Constraints for SLSQP
+    # cons = ({'type': 'eq', 'fun': prob}, {'type': 'ineq', 'fun': lambda a: a[0] - a[1]})
+
+    # Constraints for trust-constr
+    nlc = opt.NonlinearConstraint(prob, lb, 1)
+    lc = opt.LinearConstraint([1, -1], 0, np.inf)
+
+    res = opt.minimize(of, a0, constraints=[nlc, lc], bounds=bnds, method='trust-constr', options={'verbose': 1, 'initial_constr_penalty': 10})
+    print(res)
     return res
 
 bandwidths(f, g, mu, 200)
