9cd98ed303d5f0f50aacf91254d17eb41fe28beb
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2021-01-17 11:03:15 -0500

    [ 172 ] organized tex and file structure

--- 172/lab-1/CodingBat.java
+++ 172/lab-1/CodingBat.java
@@ -4,25 +4,31 @@ public class CodingBat {
   public int countPairs(String str) {
     if (str.length() <= 2) {
       return 0;
-    } else if (str.charAt(0) == str.charAt(2)) {
+    }
+    else if (str.charAt(0) == str.charAt(2)) {
       return 1 + countPairs(str.substring(1));
-    } else {
-      return countPairs(str.substring(1);
+    }
+    else {
+      return countPairs(str.substring(1));
     }
   }
 
   public int countHi2(String str) {
     if (str.length() == 1 || str.length() == 0) {
       return 0;
-    } else if (str.charAt(0) == 'x') {
+    }
+    else if (str.charAt(0) == 'x') {
       if (str.charAt(1) == 'h' && str.charAt(2) == 'i') {
         return countHi2(str.substring(2));
-      } else {
+      }
+      else {
         return countHi2(str.substring(1));
       }
-    } else if (str.charAt(0) == 'h' && str.charAt(1) == 'i') {
+    }
+    else if (str.charAt(0) == 'h' && str.charAt(1) == 'i') {
       return 1 + countHi2(str.substring(1));
-    } else {
+    }
+    else {
       return countHi2(str.substring(1));
     }
   }
@@ -30,9 +36,12 @@ public class CodingBat {
   public int countAbc(String str) {
     if (str.length() == 0 || str.length() == 1 || str.length() == 2) {
       return 0;
-    } else if (str.charAt(0) == 'a' && str.charAt(1) == 'b' && (str.charAt(2) == 'c' || str.charAt(2) == 'a')) {
+    }
+    else if (str.charAt(0) == 'a' && str.charAt(1) == 'b' &&
+             (str.charAt(2) == 'c' || str.charAt(2) == 'a')) {
       return 1 + countAbc(str.substring(1));
-    } else {
+    }
+    else {
       return countAbc(str.substring(1));
     }
   }
@@ -65,10 +74,12 @@ public class CodingBat {
   public boolean splitArray(int[] nums) {
     return splitArrayAux(nums, 0, 0, 0);
   }
+
   public boolean splitArrayAux(int [] nums, int start, int first, int second) {
     if (start == nums.length) {
       return first == second;
-    } else {
+    }
+    else {
       return splitArrayAux(nums, start + 1, first + nums[start], second)
       || splitArrayAux(nums, start + 1, first, second + nums[start]);
     }
@@ -77,10 +88,12 @@ public class CodingBat {
   public boolean splitOdd10(int[] nums) {
     return splitOdd10Aux(nums, 0, 0, 0);
   }
+
   public boolean splitOdd10Aux(int [] nums, int start, int first, int second) {
     if (start == nums.length) {
       return (first % 10 == 0) && (second % 2 != 0);
-    } else {
+    }
+    else {
       return splitOdd10Aux(nums, start + 1, first + nums[start], second) ||
       splitOdd10Aux(nums, start + 1, first, second + nums[start]);
     }
@@ -89,6 +102,7 @@ public class CodingBat {
   public boolean groupSumClump(int start, int[] nums, int target) {
     if (start >= nums.length)
       return target == 0;
+
     int sum = 0;
     int i;
     for (i = start; i < nums.length; i++) {
@@ -98,20 +112,23 @@ public class CodingBat {
         break;
     }
     return groupSumClump(i, nums, target - sum)
-    || groupSumClump(i, nums, target);
+           || groupSumClump(i, nums, target);
   }
- 
+
   public boolean groupSum5(int start, int[] nums, int target) {
     if (start == nums.length) {
       return target == 0;
-    } else {
+    }
+    else {
       if (nums[start] % 5 == 0) {
         return groupSum5(start + 1, nums, target - nums[start]);
-      } else if (start > 0 && nums[start] == 1 && nums[start - 1] % 5 == 0) {
+      }
+      else if (start > 0 && nums[start] == 1 && nums[start - 1] % 5 == 0) {
         return groupSum5(start + 1, nums, target);
-      } else {
+      }
+      else {
         return groupSum5(start + 1, nums, target - nums[start])
-        || groupSum5(start + 1, nums, target);
+               || groupSum5(start + 1, nums, target);
       }
     }
   }
@@ -119,17 +136,21 @@ public class CodingBat {
   public boolean split53(int[] nums) {
     return split53Aux(nums, 0, 0, 0);
   }
+
   public boolean split53Aux(int [] nums, int start, int first, int second) {
     if (start == nums.length) {
       return first == second;
-    } else {
+    }
+    else {
       if (nums[start] % 5 == 0) {
         return split53Aux(nums, start + 1, first + nums[start], second);
-      } else if (nums[start] % 3 == 0) {
+      }
+      else if (nums[start] % 3 == 0) {
         return split53Aux(nums, start + 1, first, second + nums[start]);
-      } else {
+      }
+      else {
         return split53Aux(nums, start + 1, first + nums[start], second)
-        || split53Aux(nums, start + 1, first, second + nums[start]);
+               || split53Aux(nums, start + 1, first, second + nums[start]);
       }
     }
   }
